6be58d57b1ac8398e2793288a2d7d7f4cf0e61bc
inf17101 <42932060+inf17101@users.noreply.github.com>
2021-11-07 20:26:37 +0100

    Update Main.cs

--- CpImportExportLibrary/src/Main.cs
+++ CpImportExportLibrary/src/Main.cs
@@ -33,9 +33,9 @@ namespace CpExportImport
             };
 
             // parse all data and make it ready for export
-            string ip = "1.16.2.230";
+            string ip = "";
             string port = "443";
-            Session session = new Session(ip, port, "oklapper", "hallo123");
+            Session session = new Session(ip, port, "username", "1234567");
             session.Login();
 
             List<string> itemsToRemove = new List<string>() { "uid", "tags", "domain", "icon", "meta-info", "read-only", "install-on", "custom-fields", "time" };
