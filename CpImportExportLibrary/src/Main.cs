0925c47f1c61bb0f8da33d2c6808d446b66555cc
inf17101 <oliver.klapper@web.de>
2021-11-13 20:13:27 +0100

    refractors code

--- CpImportExportLibrary/src/Main.cs
+++ CpImportExportLibrary/src/Main.cs
@@ -35,7 +35,7 @@ namespace CpExportImport
             // parse all data and make it ready for export
             string ip = "1.16.2.230";
             string port = "443";
-            Session session = new Session(ip, port, "oklapper", "hallo123");
+            Session session = new Session(ip, port, "username", "password");
             session.Login();
 
             List<string> itemsToRemove = new List<string>() { "uid", "tags", "domain", "icon", "meta-info", "read-only", "install-on", "custom-fields", "time" };
